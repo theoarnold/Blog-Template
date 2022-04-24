33a92faf96fd97ee96764a37c00e4268ebabc11d
theoarnold <thezzomail@gmail.com>
2022-04-24 06:01:52 +0100

    Update __init__.py

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,12 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '552ba926b6303eb3593c9a63fdbd1e90ced12f47111c9786'
+app.config['SECRET_KEY'] = ''
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1847553:Blogdatabase456@csmysql.cs.cf.ac.uk:3306/c1847553_blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 db = SQLAlchemy(app)
 
 
